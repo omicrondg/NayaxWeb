e1e9c81b7f698c72d58d91ce0497ee547c28096d
Dmitry Gurevich <dmitry.n.gurevich@gmail.com>
2018-07-12 21:52:25 +0300

    Nayax Machines

---
+++ assets/files/records-mock.ts
@@ -0,0 +1,81 @@
+import { Record } from "../../app/model/record";
+
+export const RECORDS: Record[] = [
+        {
+            id: 22,
+            machineId: 1,
+            cellIndex: "A1",
+            productCategory: "tictak",
+            pictureId: 183,
+            picture: null,
+            timestamp: "2018-07-02T07:53:12.816543",
+            modelId: 0,
+            imageStamp: 1531387129.84
+        },
+        {
+            id: 23,
+            machineId: 1,
+            cellIndex: "A1",
+            productCategory: "tictak",
+            pictureId: 184,
+            picture: null,
+            timestamp: "2018-07-02T09:13:36.1249923",
+            modelId: 0,
+            imageStamp: 1531387129.84
+        },
+        {
+            id: 24,
+            machineId: 1,
+            cellIndex: "A1",
+            productCategory: "tictak",
+            pictureId: 185,
+            picture: null,
+            timestamp: "2018-07-05T07:41:48.126168",
+            modelId: 0,
+            imageStamp: 1531387129.84
+        },
+        {
+            id: 25,
+            machineId: 1,
+            cellIndex: "A1",
+            productCategory: "greenchips",
+            pictureId: 186,
+            picture: null,
+            timestamp: "2018-07-05T08:04:49.8892578",
+            modelId: 0,
+            imageStamp: 1531387380.21
+        },
+        {
+            id: 26,
+            machineId: 1,
+            cellIndex: "A1",
+            productCategory: "greenchips",
+            pictureId: 187,
+            picture: null,
+            timestamp: "2018-07-05T08:07:17.6220044",
+            modelId: 0,
+            imageStamp: 1531387380.21
+        },
+        {
+            id: 27,
+            machineId: 1,
+            cellIndex: "A1",
+            productCategory: "tictak",
+            pictureId: 188,
+            picture: null,
+            timestamp: "2018-07-05T08:08:19.7074594",
+            modelId: 0,
+            imageStamp: 1531387380.21
+        },
+        {
+            id: 28,
+            machineId: 1,
+            cellIndex: "A1",
+            productCategory: "greenchips",
+            pictureId: 189,
+            picture: null,
+            timestamp: "2018-07-05T08:11:27.9739374",
+            modelId: 0,
+            imageStamp: 1531388250.71
+        }
+    ];
